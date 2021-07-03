5f5136dbbdc7f5f99f0d084ec240e9a61c94bddb
mohanbarman <mohanbarman9711@gmail.com>
2021-07-03 18:00:04 +0530

    Adding : Avatar component

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
+import avatarImg from "./assets/portrait.png";
+import "./avatar.css";
 
-
-export function Avatar(){
-
-    return (
-        <div>
-            Avatar
-        </div>
-    )
+export function Avatar() {
+  return (
+    <img
+      src={avatarImg}
+      className="project--avatar-img"
+      alt={"User's Avatar"}
+    />
+  );
 }
