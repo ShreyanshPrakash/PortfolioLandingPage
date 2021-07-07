189d8153f003e421378a5d14b279340f99c51597
Valli Avudaiappan <78044816+CodeFriend148@users.noreply.github.com>
2021-07-07 12:08:16 -0400

    Update Description.js

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
-
 export function Description(){
-
-    return (
-        <div>
-            Description
+    
+return (
+<div className="DesSection">
+  <div className="container">
+    <div className="des_inner">
+        <div className="Col1"></div>
+        <div className="Col2">
+            <div className="box">
+            <p>
+            I'm a software engineer located in SanFrancisco. <br />
+            <br /> I'm a co-creator of and software engineer at  <a href="https://spacecraft-repl.com" target="_blank" rel="noreferrer"><b>SpaceCraft</b></a> - an open-source, in-browser REPL
+            (Read-Evaluate-Print-Loop) where users can 
+            collaborate together in real time
+            </p>
+            </div>  
         </div>
+     </div>
+   </div>
+</div>
     )
 }
